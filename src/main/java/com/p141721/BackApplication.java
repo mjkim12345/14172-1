6f503e6bc933318f89df77c658eedb529a5bdc0a
user <offonoff6101@naver.com>
2026-05-20 09:07:41 +0900

    1

---
+++ src/main/java/com/p141721/BackApplication.java
@@ -0,0 +1,13 @@
+package com.p141721;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p141721/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p141721;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
